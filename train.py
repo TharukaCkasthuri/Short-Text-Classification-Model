cb4fd5719f6c3e97efd012c2e02585602bb7efd6
Tharuka <tharuka@Tharukas-MacBook-Air.local>
2022-05-04 15:16:03 +0530

    revised changes

--- train.py
+++ train.py
@@ -198,30 +198,13 @@ def evaluation_step(dataloader, model, loss_fn):
                 
     return acc, f1, loss
 
-def train(base_dir, train_loader, val_loader, model_name, bert_version, num_of_epochs, if_freeze_bert):
+def train(base_dir, train_loader, val_loader, model, num_of_epochs, optimizer, loss_fn, if_freeze_bert):
     """
     """
-
-    device = setup()
-
-    if (model_name == 'BertWithLSTMClassifier'):
-        model = models.BertWithLSTMClassifier(77, bert_version)
-    elif (model_name == 'BertWithBiLSTMClassifier'):
-        model = models.BertWithBiLSTMClassifier(77, bert_version)
-    else:
-        pass
-    model.to(device)
-
-    optimizer = AdamW(model.parameters(), lr=hparams["learning_rate"])
-    logging.info('Initialized optimizer.')
-    print('Initialized optimizer.')
-    loss_fn = torch.nn.CrossEntropyLoss()
-    logging.info('Initialized optimizer.')
-    print('Initialized loss function.')
 
     tqdm.pandas()
     best_acc, best_f1 = 0, 0
-    model_file_name = str(time.time()) +str(model_name)+"_"+str(bert_version)+ "_trained_model.pt"
+    model_file_name = str(time.time()) + "_trained_model.pt"
     path = os.path.join(base_dir,model_file_name)
 
     train_f1s = []
@@ -268,7 +251,7 @@ def train(base_dir, train_loader, val_loader, model_name, bert_version, num_of_e
     plt.plot(range(1, num_of_epochs+1), validation_losses, 'c', label='Validation Loss Curve')
     plt.title("Training/Validation F1 Score/Loss Curves")
     plt.legend()
-    plot_file = str(time.time()) +str(model_name)+"_"+str(bert_version)+"_plot.png"
+    plot_file = str(time.time())+"_plot.png"
     #plot_path = os.path.join(base_dir,plot_file)
     plt.savefig(os.path.join(base_dir,plot_file))
 
@@ -309,8 +292,24 @@ if __name__ == '__main__':
                     bert_model = bert_model,
                     hparams = hparams)
 
-    train(base_dir, train_data, validation_data, classifier,bert_model, 20, False)
+    device = setup()
 
+    if (classifier == 'BertWithLSTMClassifier'):
+        cls_model = models.BertWithLSTMClassifier(77,bert_model)
+    elif (classifier == 'BertWithBiLSTMClassifier'):
+        cls_model = models.BertWithBiLSTMClassifier(77,bert_model)
+    else:
+        pass
+    cls_model.to(device)
+
+    optimizer = AdamW(cls_model.parameters(), lr=hparams["learning_rate"])
+    logging.info('Initialized optimizer.')
+    print('Initialized optimizer.')
+    loss_fn = torch.nn.CrossEntropyLoss()
+    logging.info('Initialized optimizer.')
+    print('Initialized loss function.')
+
+    train(base_dir, train_data, validation_data, cls_model, 20, optimizer, loss_fn, False)
 
 
 
